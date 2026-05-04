fe2a79453bc5eaeee319a740bbd3c8a10059e5ec
Brook <30013859@cc.peralta.edu>
2026-05-04 22:48:22 +0000

    Add private members to GradeBook header

---
+++ GradeBook.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+ 
+class GradeBook {
+public:
+    GradeBook();
+    void addGrade(const std::string& assignment, double score);
+    double getAverage() const;
+    double getHighest() const;
+    int numGrades() const;
+private:
+    // You decide what goes here.
+     std::vector<std::string> assignments;
+    std::vector<double> scores;
+};
