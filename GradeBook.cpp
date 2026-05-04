bca220bcdc1db413669ec0878a60f0c4a3a85ce7
Brook <30013859@cc.peralta.edu>
2026-05-04 22:54:54 +0000

    Add stub implementations for all GradeBook functions

---
+++ GradeBook.cpp
@@ -0,0 +1,20 @@
+#include "GradeBook.h"
+
+GradeBook::GradeBook() {
+}
+
+void GradeBook::addGrade(const std::string& assignment, double score) {
+    
+}
+
+double GradeBook::getAverage() const {
+    return 0.0; 
+}
+
+double GradeBook::getHighest() const {
+    return 0.0; 
+}
+
+int GradeBook::numGrades() const {
+    return 0; 
+}
